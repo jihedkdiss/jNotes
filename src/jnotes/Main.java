4112e7a06b9b0c2c002ffb25303f7ce7dcc6eebb
Jihed Kdiss <jihedkdiss@outlook.com>
2021-05-13 01:05:54 +0100

    Added printBanner()

--- src/jnotes/Main.java
+++ src/jnotes/Main.java
@@ -2,8 +2,17 @@ package jnotes;
 
 public class Main {
 
+    public static void printBanner() {
+        String banner = "    _ _   _       _                \n" +
+                        "   (_) \\ | | ___ | |_ ___  ___    \n" +
+                        "   | |  \\| |/ _ \\| __/ _ \\/ __| \n" +
+                        "   | | |\\  | (_) | ||  __/\\__ \\ \n" +
+                        "  _/ |_| \\_|\\___/ \\__\\___||___/\n" +
+                        " |__/      Coded by @jihedkdiss";
+        System.out.println(banner);
+    }
+
     public static void main(String[] args) {
-        // Hello World!
-  	    System.out.println("Hello World!");
+        printBanner();
     }
 }
