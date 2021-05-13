b6d732beab30ce73eedf7f404fe4e7e5645a9034
Jihed Kdiss <jihedkdiss@outlook.com>
2021-05-13 10:36:57 +0100

    Put everything in while() loop & add waitForUser()

--- src/jnotes/Main.java
+++ src/jnotes/Main.java
@@ -25,6 +25,22 @@ public class Main {
         System.out.println("  6. Close");
     }
 
+    private static void waitForUser() throws IOException, InterruptedException {
+        String os = System.getProperty("os.name");
+        if (os.equals("Linux")) {
+            System.out.println("      Press enter key to continue...");
+            //noinspection Since15
+            new ProcessBuilder("read").inheritIO().start().waitFor();
+        } else if (os.contains("Windows")) {
+            System.out.println();
+            //noinspection Since15
+            new ProcessBuilder("cmd", "/c", "pause").inheritIO().start().waitFor();
+        } else {
+            System.out.println("  [ Error ] System not recognised. Leaving...");
+            System.exit(1);
+        }
+    }
+
     private static void clearScreen() throws IOException, InterruptedException {
         String os = System.getProperty("os.name");
         if (os.equals("Linux")) {
@@ -46,74 +62,84 @@ public class Main {
             notes[i] = "(empty)";
         }
         int cursor = 0;
-        clearScreen();
-        printBanner();
-        printOptions();
-        System.out.println();
-        System.out.print("  >> ");
-        int option = input.nextInt();
-        input.nextLine();
-        System.out.println();
-        switch (option) {
-            case 1:
-                System.out.println("  [ Add Note ] Type new note...");
-                System.out.println();
-                System.out.print("  >> ");
-                String newNote = input.nextLine();
-                notes[cursor] = newNote;
-                cursor++;
-                System.out.println();
-                System.out.println("  [i] Note added successfully!");
-                break;
-            case 2:
-                System.out.println("  [ Remove Note ] Type note number...");
-                System.out.println();
-                System.out.print("  >> ");
-                int noteNumber = input.nextInt();
-                input.nextLine();
-                notes[noteNumber - 1] = "(removed)";
-                System.out.println();
-                System.out.println("  [i] Note removed successfully!");
-                break;
-            case 3:
-                System.out.println("  [ Edit Note ] Type note number...");
-                System.out.println();
-                System.out.print("  >> ");
-                int noteNumber2 = input.nextInt();
-                input.nextLine();
-                System.out.println();
-                System.out.println("  [ Edit Note ] Type new note...");
-                System.out.println();
-                System.out.print("  >> ");
-                String newNote2 = input.nextLine();
-                System.out.println();
-                notes[noteNumber2 - 1] = newNote2;
-                System.out.println("  [i] Note edited successfully!");
-                break;
-            case 4:
-                System.out.println("  [ Clear Notes ] Clearing notes...");
-                System.out.println();
-                for (int i = 0; i < notes.length; i++) {
-                    notes[i] = "(empty)";
-                }
-                cursor = 0;
-                System.out.println("  [i] Notes cleared successfully!");
-                break;
-            case 5:
-                System.out.println("  [ Print Notes ] Printing notes...");
-                System.out.println();
-                for (int i = 0; i < notes.length; i++) {
-                    if (!notes[i].equals("(empty)")) {
-                        System.out.println("  " + i + 1 + ". " + notes[i]);
+
+        while(true) {
+            clearScreen();
+            printBanner();
+            printOptions();
+            System.out.println();
+            System.out.print("  >> ");
+            int option = input.nextInt();
+            input.nextLine();
+            System.out.println();
+            switch (option) {
+                case 1:
+                    System.out.println("  [ Add Note ] Type new note...");
+                    System.out.println();
+                    System.out.print("  >> ");
+                    String newNote = input.nextLine();
+                    notes[cursor] = newNote;
+                    cursor++;
+                    System.out.println();
+                    System.out.println("  [i] Note added successfully!");
+                    Thread.sleep(2000);
+                    break;
+                case 2:
+                    System.out.println("  [ Remove Note ] Type note number...");
+                    System.out.println();
+                    System.out.print("  >> ");
+                    int noteNumber = input.nextInt();
+                    input.nextLine();
+                    notes[noteNumber - 1] = "(removed)";
+                    System.out.println();
+                    System.out.println("  [i] Note removed successfully!");
+                    Thread.sleep(2000);
+                    break;
+                case 3:
+                    System.out.println("  [ Edit Note ] Type note number...");
+                    System.out.println();
+                    System.out.print("  >> ");
+                    int noteNumber2 = input.nextInt();
+                    input.nextLine();
+                    System.out.println();
+                    System.out.println("  [ Edit Note ] Type new note...");
+                    System.out.println();
+                    System.out.print("  >> ");
+                    String newNote2 = input.nextLine();
+                    System.out.println();
+                    notes[noteNumber2 - 1] = newNote2;
+                    System.out.println("  [i] Note edited successfully!");
+                    Thread.sleep(2000);
+                    break;
+                case 4:
+                    System.out.println("  [ Clear Notes ] Clearing notes...");
+                    System.out.println();
+                    for (int i = 0; i < notes.length; i++) {
+                        notes[i] = "(empty)";
                     }
-                System.out.println("  [i] Notes printed successfully!");
-                }
-                break;
-            case 6:
-                clearScreen();
-                System.exit(0);
-                break;
+                    cursor = 0;
+                    System.out.println("  [i] Notes cleared successfully!");
+                    Thread.sleep(2000);
+                    break;
+                case 5:
+                    System.out.println("  [ Print Notes ] Printing notes...");
+                    System.out.println();
+                    for (int i = 0; i < notes.length; i++) {
+                        if (!notes[i].equals("(empty)")) {
+                            System.out.println("  " + i + 1 + ". " + notes[i]);
+                        }
+                    }
+                    System.out.println("  -- END --");
+                    System.out.println();
+                    System.out.println("  [i] Notes printed successfully!");
+                    waitForUser();
+                    break;
+                case 6:
+                    clearScreen();
+                    System.exit(0);
+                    break;
+            }
+            clearScreen();
         }
-        clearScreen();
     }
 }
